7a9ea7bd7114ff60240bbeb8e1048cc22404c1ae
Gabriel Salazar <gabriel.salazar777@gmail.com>
2023-06-06 19:02:55 -0400

    calendar and color functionality

--- models/Project.js
+++ models/Project.js
@@ -1,44 +1,48 @@
-const { model, Schema } = require("mongoose");
+const { model, Schema } = require('mongoose');
 
 const projectSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    status: {
-      type: Boolean,
-      //   required: true,
-      default: false,
-    },
-    projectType: {
-      type: [String],
-      required: true,
-    },
-    notes: {
-      type: String,
-      default: "",
-    },
-    client: {
-      type: Schema.Types.ObjectId,
-      ref: "Client",
-      required: true,
-    },
-    materials: [
-      {
-        name: { type: String, required: true, default: "" },
-        squareFeet: { type: Number, required: true, default: 0 },
-        coverage: { type: Number, required: true, default: 0 },
-        units: { type: Number, required: true, default: 0 },
+   {
+      name: {
+         type: String,
+         required: true,
       },
-    ],
-    startDate: Date,
-    endDate: Date,
-  },
-  {
-    timestamps: true,
-    timeseries: true,
-  }
+      status: {
+         type: Boolean,
+         //   required: true,
+         default: false,
+      },
+      projectType: {
+         type: [String],
+         required: true,
+      },
+      notes: {
+         type: String,
+         default: '',
+      },
+      client: {
+         type: Schema.Types.ObjectId,
+         ref: 'Client',
+         required: true,
+      },
+      materials: [
+         {
+            name: { type: String, required: true, default: '' },
+            squareFeet: { type: Number, required: true, default: 0 },
+            coverage: { type: Number, required: true, default: 0 },
+            units: { type: Number, required: true, default: 0 },
+         },
+      ],
+      startDate: Date,
+      endDate: Date,
+      color: {
+         type: String,
+         default: '#12c946',
+      },
+   },
+   {
+      timestamps: true,
+      timeseries: true,
+   }
 );
 
-module.exports = model("Project", projectSchema);
+module.exports = model('Project', projectSchema);
--- routes/projects.js
+++ routes/projects.js
@@ -1,75 +1,100 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const Project = require("../models/Project");
-const Client = require("../models/Client");
+const Project = require('../models/Project');
+const Client = require('../models/Client');
 
-const isAuthenticated = require("../middleware/isAuthenticated");
+const isAuthenticated = require('../middleware/isAuthenticated');
 
 //CREATE
-router.post("/create", isAuthenticated, (req, res) => {
-  Project.create(req.body)
-    .then((createdProject) => {
-      Project.findById(createdProject._id)
-        .populate("client")
-        .then((foundProject) => {
-          console.log("PROJECT CREATED: ", foundProject);
-          res.json(foundProject);
-        });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+router.post('/create', isAuthenticated, (req, res) => {
+   Project.create(req.body)
+      .then((createdProject) => {
+         Project.findById(createdProject._id)
+            .populate('client')
+            .then((foundProject) => {
+               console.log('PROJECT CREATED: ', foundProject);
+               res.json(foundProject);
+            });
+      })
+      .catch((err) => {
+         console.log(err);
+      });
 });
 
 //READ ALL
-router.get("/display", isAuthenticated, (req, res) => {
-  Project.find()
-    .populate("client")
-    .then((foundProjects) => {
-      console.log("FOUND ALL PROJECTS: ", foundProjects);
-      res.json(foundProjects);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+router.get('/display', isAuthenticated, (req, res) => {
+   Project.find()
+      .populate('client')
+      .then((foundProjects) => {
+         console.log('FOUND ALL PROJECTS: ', foundProjects);
+         res.json(foundProjects);
+      })
+      .catch((err) => {
+         console.log(err);
+      });
 });
 
 //READ ONE
-router.get("/display/:projectId", isAuthenticated, (req, res) => {
-  Project.findById(req.params.projectId)
-    .populate("client")
-    .then((foundProject) => {
-      console.log("FOUND ONE PROJECTS: ", foundProject);
-      res.json(foundProject);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+router.get('/display/:projectId', isAuthenticated, (req, res) => {
+   Project.findById(req.params.projectId)
+      .populate('client')
+      .then((foundProject) => {
+         console.log('FOUND ONE PROJECTS: ', foundProject);
+         res.json(foundProject);
+      })
+      .catch((err) => {
+         console.log(err);
+      });
 });
 
 //UPDATE ONE
-router.post("/edit/:projectId", isAuthenticated, (req, res) => {
-  Project.findByIdAndUpdate(req.params.projectId, req.body, { new: true })
-    .populate("client")
-    .then((updatedProject) => {
-      res.json(updatedProject);
-      console.log("EDIT: ", updatedProject);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+router.post('/edit/:projectId', isAuthenticated, (req, res) => {
+   Project.findByIdAndUpdate(req.params.projectId, req.body, { new: true })
+      .populate('client')
+      .then((updatedProject) => {
+         res.json(updatedProject);
+         console.log('EDIT: ', updatedProject);
+      })
+      .catch((err) => {
+         console.log(err);
+      });
 });
 
 //DELETE ONE
-router.get("/delete/:projectId", isAuthenticated, (req, res) => {
-  Project.findByIdAndDelete(req.params.projectId)
-    .then((deletedResult) => {
-      res.json(deletedResult);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+router.get('/delete/:projectId', isAuthenticated, (req, res) => {
+   Project.findByIdAndDelete(req.params.projectId)
+      .then((deletedResult) => {
+         res.json(deletedResult);
+      })
+      .catch((err) => {
+         console.log(err);
+      });
+});
+
+//GET PROJECT DATES
+router.get('/dates', isAuthenticated, (req, res) => {
+   Project.find({
+      $and: [{ startDate: { $exists: true } }, { endDate: { $exists: true } }],
+   })
+      .select('name startDate endDate color')
+      .then((projectsWithDates) => {
+         res.json(projectsWithDates);
+      })
+      .catch((err) => {
+         console.log(err);
+      });
+});
+
+// UPDATE PROJECT COLOR
+router.post('/dates', isAuthenticated, (req, res) => {
+   Project.findByIdAndUpdate(req.body.id, { color: req.body.color }, { new: true })
+      .then((updatedProject) => {
+         res.json(updatedProject);
+      })
+      .catch((err) => {
+         console.log(err);
+      });
 });
 
 module.exports = router;
